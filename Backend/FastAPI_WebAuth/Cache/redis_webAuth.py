f28d8877d90997a5ae6ba81e05a57a6dffc3cf67
devs <wwdevkhati2@gmail.com>
2026-09-12 20:56:14 +0530

    Fix parameter name in redis set function for OTP expiration in Set_email_with_otp function.

--- Backend/FastAPI_WebAuth/Cache/redis_webAuth.py
+++ Backend/FastAPI_WebAuth/Cache/redis_webAuth.py
@@ -5,9 +5,8 @@ redis = Redis.from_url(REDIS_URL , decode_responses=True )
 
 def Set_email_with_otp(Email:str , otp:str , Expiry_in_Sec :int = 300)  -> dict:
     try:
-        redis.set(Email,otp ,Exp)
+        redis.set(Email,otp ,ex=Expiry_in_Sec)
         return {"operation":True}
     except Exception as e:
         return {"operation":False , "error":str(e)}
         
-    
