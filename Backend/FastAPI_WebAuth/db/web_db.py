b8388b08ba55673c735744b6f7f0a39885afdb13
devs <wwdevkhati2@gmail.com>
2026-09-12 18:06:17 +0530

    Refactor web_db.py: Update imports, add logging configuration, and ensure DATABASE_URL is validated.

--- Backend/FastAPI_WebAuth/db/web_db.py
+++ Backend/FastAPI_WebAuth/db/web_db.py
@@ -1,21 +1,24 @@
 from sqlalchemy import create_engine , column 
-from sqlalchemy.orm import mapped_column ,Mapped ,Session ,declarative_base
+from sqlalchemy.orm import mapped_column ,Mapped ,Session , DeclarativeBase
 
 import os 
 from dotenv import load_dotenv
+ import logging
 
 load_dotenv()
+logging.basicConfig(level=)
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise RuntimeError ("Database url is missing")
 
 engine = create_engine(DATABASE_URL)
 
 
-class Base (declarative_base):
+class Base (DeclarativeBase):
     pass
 
 
-if not DATABASE_URL:
-    raise RuntimeError ("Database url is missing")
 
 
