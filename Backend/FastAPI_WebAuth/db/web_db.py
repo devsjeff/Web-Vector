8014b67b6222ac0c4f84bd2ec900090c77b732f8
devs <wwdevkhati2@gmail.com>
2026-09-12 19:56:13 +0530

    Refactor database user functions to use context manager for session handling and update email/password field constraints.

--- Backend/FastAPI_WebAuth/db/web_db.py
+++ Backend/FastAPI_WebAuth/db/web_db.py
@@ -26,8 +26,8 @@ class DatabaseSchema(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     name :Mapped[str] = mapped_column(String(20),nullable=True)
     last_name :Mapped[str] = mapped_column(String(20),nullable=True)
-    email :Mapped[str]  = mapped_column(String(50) , nullable=False)  
-    password : Mapped[str] = mapped_column(String(100) , nullable=False)
+    email :Mapped[str]  = mapped_column(String(50) , unique=True,nullable=False)  
+    password : Mapped[str] = mapped_column(String(255) , nullable=False)
 
 
 
@@ -35,32 +35,30 @@ Base.metadata.create_all(engine)
 
 
 def CheckUserExistOrNot(email):
-    
-    try:
-        Session_use = Session(bind=engine)
-        EmailExist = Session_use.query(DatabaseSchema).filter(DatabaseSchema.email == email).first()
-        if not EmailExist:
-            return {"operation_success":True , "UserExist":False}
-        else :
-            return {"operation_success":True , "UserExist":True}
-    except:
-        return {"operation_success":False}
-    finally :
-        Session_use.close()
+    with Session(engine) as Session_use:
+        try:
+            
+            EmailExist = Session_use.query(DatabaseSchema).filter(DatabaseSchema.email == email).first()
+            if not EmailExist:
+                return {"operation_success":True , "UserExist":False}
+            else :
+                return {"operation_success":True , "UserExist":True}
+        except:
+            return {"operation_success":False}
+        
         
        
     
 
 def CreateUserAccount (email , password):
-    try :
-        Session_use  = Session(bind=engine)
-        Create_User = DatabaseSchema(email = email , password = password)
-        Session_use.add(Create_User)
-        Session_use.commit()
-        return  {"operation_success":True}
-    except:
-         Session_use.rollback()
-         return {"operation_success":False}
-    finally:
-        Session_use.close()
+    with Session (engine) as Session_use :
+        try :
+            Session_use  = Session(bind=engine)
+            Create_User = DatabaseSchema(email = email , password = password)
+            Session_use.add(Create_User)
+            Session_use.commit()
+            return  {"operation_success":True}
+        except:
+            return {"operation_success":False}
+
     
