b43ec3b9281573be21d791c9da91ece0b1f26a84
devs <wwdevkhati2@gmail.com>
2026-09-12 18:01:18 +0530

    Auto-commit: changes

--- Backend/FastAPI_WebAuth/db/web_db.py
+++ Backend/FastAPI_WebAuth/db/web_db.py
@@ -1,6 +1,21 @@
 from sqlalchemy import create_engine , column 
 from sqlalchemy.orm import mapped_column ,Mapped ,Session ,declarative_base
 
+import os 
+from dotenv import load_dotenv
 
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
+
+
+class Base (declarative_base):
+    pass
+
+
+if not DATABASE_URL:
+    raise RuntimeError ("Database url is missing")
 
 
