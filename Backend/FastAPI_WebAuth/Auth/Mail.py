1de4ae2a6c5e11af7686ab0a8a95081a4b7304cd
devs <wwdevkhati2@gmail.com>
2026-09-12 21:56:14 +0530

    Enhance Send_otp function to include OTP in email content and set email headers.

--- Backend/FastAPI_WebAuth/Auth/Mail.py
+++ Backend/FastAPI_WebAuth/Auth/Mail.py
@@ -3,7 +3,23 @@ from Types_pydantic import Email
 import aiosmtplib
 from email.message import Message
 
-async def Send_otp(email:Email):
+Port:int = 587
+
+async def Send_otp(email:Email , otp):
     message = Message()
     
-    message[]
+    message["From"]= EMAIL_ADDRESS
+    message["To"] = email
+    message["Subject"] = " Web-Vector  Email Verification"
+    
+    message.set_content(f"""
+    Your Web Vector verification code is:
+                    {otp}
+    This code will expire in 5 minutes.
+    If you did not request this, you can ignore this email.
+    """)
+    await aiosmtplib.send(
+        message
+        
+        
+    )
