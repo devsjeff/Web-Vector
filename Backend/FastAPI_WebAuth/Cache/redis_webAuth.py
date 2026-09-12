6a55f9af97679a402daa618a47db97f3e099e9f4
devs <wwdevkhati2@gmail.com>
2026-09-12 21:11:16 +0530

    Fix OTP storage format in Set_email_with_otp function in redis_webAuth.py

--- Backend/FastAPI_WebAuth/Cache/redis_webAuth.py
+++ Backend/FastAPI_WebAuth/Cache/redis_webAuth.py
@@ -5,7 +5,7 @@ redis = Redis.from_url(REDIS_URL , decode_responses=True )
 
 async def Set_email_with_otp(Email:str , otp:str , Expiry_in_Sec :int = 300)  -> dict:
     try:
-        await redis.set(Email,otp ,ex=Expiry_in_Sec)
+        await redis.set(f"otp: {Email,otp} ,ex = {Expiry_in_Sec}")
         return {"operation":True}
     except Exception as e:
         return {"operation":False , "error":str(e)}
