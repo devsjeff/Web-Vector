984ca25d6c36489411c7c6cc691c8caa12330aab
devs <wwdevkhati2@gmail.com>
2026-09-12 21:16:14 +0530

    Fix OTP storage format and add email OTP deletion function in redis_webAuth.py.

--- Backend/FastAPI_WebAuth/Cache/redis_webAuth.py
+++ Backend/FastAPI_WebAuth/Cache/redis_webAuth.py
@@ -5,8 +5,16 @@ redis = Redis.from_url(REDIS_URL , decode_responses=True )
 
 async def Set_email_with_otp(Email:str , otp:str , Expiry_in_Sec :int = 300)  -> dict:
     try:
-        await redis.set(f"otp: {Email,otp} ,ex = {Expiry_in_Sec}")
+        await redis.set(f"otp:{Email}",otp , ex = Expiry_in_Sec)
         return {"operation":True}
     except Exception as e:
         return {"operation":False , "error":str(e)}
         
+async def Delete_Email_otp(email:str ) -> dict :
+    try :
+        await redis.delete(f"otp:{email}")
+        return {"operation":True}
+    except Exception as e :
+        return {"operation":False , "error":e}
+
+async def 
