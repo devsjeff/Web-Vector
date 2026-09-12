552ed4ee1f8b2267b3bab1c5ecc18d62c5b9a5be
devs <wwdevkhati2@gmail.com>
2026-09-12 21:51:14 +0530

    Add async Send_otp function to Mail.py for sending OTP emails

--- Backend/FastAPI_WebAuth/Auth/Mail.py
+++ Backend/FastAPI_WebAuth/Auth/Mail.py
@@ -1,4 +1,9 @@
 from FastAPI_WebAuth.Common_configs import EMAIL_ADDRESS ,EMAIL_APP_PASSWORD
-
+from Types_pydantic import Email
 import aiosmtplib
 from email.message import Message
+
+async def Send_otp(email:Email):
+    message = Message()
+    
+    message[]
