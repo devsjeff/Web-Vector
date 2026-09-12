7292e518d985cba44c8c7074fde255b19dd4f537
devs <wwdevkhati2@gmail.com>
2026-09-12 20:51:13 +0530

    Enhance Set_email_with_otp function to include expiry parameter and improve error handling.

--- Backend/FastAPI_WebAuth/Cache/redis_webAuth.py
+++ Backend/FastAPI_WebAuth/Cache/redis_webAuth.py
@@ -3,11 +3,11 @@ from FastAPI_WebAuth.Common_configs import REDIS_URL
 
 redis = Redis.from_url(REDIS_URL , decode_responses=True )
 
-def Set_email_with_otp(Email:str , otp:str) :
+def Set_email_with_otp(Email:str , otp:str , Expiry_in_Sec :int = 300)  -> dict:
     try:
-        redis.set(Email,otp)
-        return True
+        redis.set(Email,otp ,Exp)
+        return {"operation":True}
     except Exception as e:
-        return e
+        return {"operation":False , "error":str(e)}
         
     
