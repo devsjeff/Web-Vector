85e941f3de6234b9feff8c52c51f4d54946b9f82
devs <wwdevkhati2@gmail.com>
2026-09-12 20:46:14 +0530

    Add function to set email with OTP in Redis cache

--- Backend/FastAPI_WebAuth/Cache/redis_webAuth.py
+++ Backend/FastAPI_WebAuth/Cache/redis_webAuth.py
@@ -2,3 +2,12 @@ from redis.asyncio import Redis
 from FastAPI_WebAuth.Common_configs import REDIS_URL
 
 redis = Redis.from_url(REDIS_URL , decode_responses=True )
+
+def Set_email_with_otp(Email:str , otp:str) :
+    try:
+        redis.set(Email,otp)
+        return True
+    except Exception as e:
+        return e
+        
+    
